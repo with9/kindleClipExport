026294f80ebe3e07d51db5363594ddd821f8e205
with9_from_Arch <dc198424601@outlook.com>
2020-04-22 21:40:22 +0800

    to markdown type

---
+++ README.md
@@ -0,0 +1,8 @@
+# kindleClipExport
+easy way to export the Kindle's My Clippings.txt
+
+Step 
+- copy My Clipping.txt to this root dir
+- add 'RealEnd' at the end of the file
+- mkdir output
+- python clip.py
--- clip.py
+++ clip.py
@@ -1,13 +1,36 @@
 book_list=[]
 book_flag=0
+fl=None
+wordlist=[]
+def writeLine(linelist):
+    """
+    linelist[0]:booktitle
+    linelist[1]:the msg of bookmark
+    linelist[2:]:content
+    """
+    filename='output/'+linelist[0]+'.md'
+    with open(filename,'a')as f:
+        if '标注'in linelist[1]:
+            f.write('>')
+        for line in linelist[2:]:
+            f.write(line)
+        f.write('\n')
+        f.write(linelist[1])
+        # f.write('\n')
 with open('My Clippings.txt','r')as f:
     while 1 :
-        line = f.readline().split('\n')[0]
-            if book_flag==1:
-                if line != " ":
-                    print(line)
-                book_flag=0
-            if line == "==========":
-                book_flag=1
-            if line == "RealEnd":#defind the end of the file
-                break
+        line = f.readline()
+        if '-' in line:
+            if '书签' in line:#exclude the msg which is only about bookmark
+                wordlist=[]
+                continue
+        else:
+            line=line.split('\n')[0]
+        wordlist.append(line)
+        if line == "==========":
+            wordlist.pop()
+            writeLine(wordlist)
+            wordlist=[]
+        if line == "RealEnd":#defind the end of the file
+            break
+
